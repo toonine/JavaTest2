b0c8f4065f7ba2a61bfcb12d2fe7c1fcea220bfb
金榕 <rong.jin01@hand-china.com>
2018-07-21 12:59:56 +0800

    [ADD]提交作答

--- build.sh
+++ build.sh
@@ -1,15 +1,1 @@
 #!/usr/bin/env bash
-echo ""
-echo "---------------------------------EXAM1 RESULT------------------------------------"
-echo "`cd Exam1`"
-echo "`./Exam1/build.sh`"
-echo ""
-
-echo ""
-echo "---------------------------------EXAM2 RESULT------------------------------------"
-exec ./Exam2/build.sh
-echo ""
-
-echo ""
-echo "---------------------------------EXAM3 RESULT------------------------------------"
-exec ./Exam3/build.sh
